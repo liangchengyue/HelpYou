280108482e74865eadf4fb94c3ea4652994e9645
zrk <906031318@qq.com>
2017-05-16 15:33:34 +0800

    修改登录布局

--- campushelp/src/main/java/com/example/application/LoginActivity.java
+++ campushelp/src/main/java/com/example/application/LoginActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -27,6 +28,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         setContentView(R.layout.activity_login);
         userName=(EditText)findViewById(R.id.userName);
         password=(EditText)findViewById(R.id.password1);
