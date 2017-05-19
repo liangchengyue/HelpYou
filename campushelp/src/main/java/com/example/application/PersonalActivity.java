fe26e915584449390625ba5263c21f89118c28eb
zrk <906031318@qq.com>
2017-05-19 21:14:21 +0800

    增加新订单提醒的按钮功能

--- campushelp/src/main/java/com/example/application/PersonalActivity.java
+++ campushelp/src/main/java/com/example/application/PersonalActivity.java
@@ -5,15 +5,26 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
 
 public class PersonalActivity extends MainActivity {
-
-
+    private ToggleButton toggleButton;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_personal);
-
         super.init(2);
+        //按钮开关
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                toggleButton.setChecked(isChecked);
+                imageView.setImageResource(isChecked?R.drawable.switch_btn:R.drawable.switch_btn);
+            }
+        });
+
     }
 }
