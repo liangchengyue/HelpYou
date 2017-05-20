c8b637c2170948ee925925019238db4c5d8bf9f0
梁城月 <2524499314@qq.com>
2017-05-20 17:46:22 +0800

    注册

--- campushelp/src/main/java/com/example/application/SetpasswordActivity.java
+++ campushelp/src/main/java/com/example/application/SetpasswordActivity.java
@@ -1,8 +1,8 @@
 package com.example.application;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
