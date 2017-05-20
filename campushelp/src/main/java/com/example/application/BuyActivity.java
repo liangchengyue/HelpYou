0f4a59068b82bebf451398d251595f7cc6ce4f29
zrk <906031318@qq.com>
2017-05-20 16:47:52 +0800

    优化页面显示

--- campushelp/src/main/java/com/example/application/BuyActivity.java
+++ campushelp/src/main/java/com/example/application/BuyActivity.java
@@ -1,13 +1,26 @@
 package com.example.application;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BuyActivity extends AppCompatActivity {
-
+    //返回个人页面
+    private Button Breturn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy);
+
+        Breturn= (Button) findViewById(R.id.B_return);
+        Breturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent buy = new Intent(BuyActivity.this,MainActivity.class);
+                startActivity(buy);
+            }
+        });
     }
 }
--- campushelp/src/main/java/com/example/application/LssueActivity.java
+++ campushelp/src/main/java/com/example/application/LssueActivity.java
@@ -1,13 +1,26 @@
 package com.example.application;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LssueActivity extends AppCompatActivity {
-
+    //返回个人页面
+    private Button Lreturn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lssue);
+
+        Lreturn = (Button) findViewById(R.id.L_return);
+        Lreturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent lssue = new Intent(LssueActivity.this,MainActivity.class);
+                startActivity(lssue);
+            }
+        });
     }
 }
