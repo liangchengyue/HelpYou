f49add267b44edd727a2cf5e34cd67302a970cd6
zrk <906031318@qq.com>
2017-05-23 22:47:43 +0800

    增加商家活动页面

--- campushelp/src/main/java/com/example/application/BussinessActivity.java
+++ campushelp/src/main/java/com/example/application/BussinessActivity.java
@@ -4,10 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class BussinessActivity extends AppCompatActivity {
     //商家活动页面退出
     private Button bussOut;
+    //显示TextView合集
+    private TextView bussName;
+    private TextView bussActiv;
+    private TextView bussAddress;
+    private TextView bussTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +25,10 @@ public class BussinessActivity extends AppCompatActivity {
                 finish();
             }
         });
+        //显示TextView合集
+        bussName= (TextView) findViewById(R.id.buss_name);
+        bussActiv= (TextView) findViewById(R.id.buss_activ);
+        bussAddress= (TextView) findViewById(R.id.buss_address);
+        bussTime= (TextView) findViewById(R.id.buss_time);
     }
 }
