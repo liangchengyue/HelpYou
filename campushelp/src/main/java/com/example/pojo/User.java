1712e14ee17b3b14abdeaf6e39aa0c68d22997a5
梁城月 <2524499314@qq.com>
2017-07-16 11:17:47 +0800

    修改

---
+++ campushelp/src/main/java/com/example/pojo/User.java
@@ -0,0 +1,101 @@
+package com.example.pojo;
+
+/**
+ * Created by 梁城月 on 2017/7/16.
+ */
+
+public class User {
+    private String userName;
+    private String trueName;
+    private String password;
+    private String nickName;
+    private String address;
+    private String telephone;
+    private String gender;
+    /**
+     * 积分
+     */
+    private int integral;
+    private String idCard;
+    private String imagePath;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getTrueName() {
+        return trueName;
+    }
+
+    public void setTrueName(String trueName) {
+        this.trueName = trueName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getIntegral() {
+        return integral;
+    }
+
+    public void setIntegral(int integral) {
+        this.integral = integral;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+}
